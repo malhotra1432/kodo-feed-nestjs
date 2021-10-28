0ab29613bff1e651a8cb12eac719cad3cf6dac28
malhotra1432 <malhotra1432@gmail.com>
2021-10-29 02:29:51 +0530

    [PM] removes orm and set up custom repo

--- src/adapters/repository/feed.json.repository.ts
+++ src/adapters/repository/feed.json.repository.ts
@@ -1,16 +1,14 @@
-import { EntityRepository, Repository } from 'typeorm';
 import { FeedEntity } from '../entity/FeedEntity';
 import { readFile, writeFile } from 'fs/promises';
 
-@EntityRepository(FeedEntity)
-export class FeedOrmRepository extends Repository<FeedEntity> {
-  public async saveDataToJsonFile(data: Array<FeedEntity>) {
+export class FeedOrmRepository {
+  static async save(data: Array<FeedEntity>) {
     await writeFile('./feed.json', JSON.stringify(data), {
       encoding: 'utf8',
     });
   }
 
-  async findAll(): Promise<Array<FeedEntity>> {
+  static async findAll(): Promise<Array<FeedEntity>> {
     const jsonData = await readFile('./feed.json');
     return JSON.parse(jsonData.toString());
   }
